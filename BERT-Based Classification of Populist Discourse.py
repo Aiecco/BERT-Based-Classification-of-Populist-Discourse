34e380cec930f3c9ddfc3b810d2d024d38826a90
Alessandro Pala <97117436+Aiecco@users.noreply.github.com>
2024-05-05 12:16:10 +0200

    Update BERT-Based Classification of Populist Discourse.py

--- BERT-Based Classification of Populist Discourse.py
+++ BERT-Based Classification of Populist Discourse.py
@@ -2,18 +2,27 @@
 import torch
 from torch.optim import AdamW
 from torch.utils.data import DataLoader, Dataset
-from transformers import BertTokenizer, BertForSequenceClassification
-import pandas as pd
+
 from sklearn.model_selection import train_test_split
+
+from transformers import BertTokenizer, BertForSequenceClassification
+
+import pandas as pd
+
 import matplotlib.pyplot as plt
-from IPython.display import display, clear_output
+import seaborn as sns
+from IPython.display import clear_output
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#%%
+
+
+
 n_epochs = 50
 l_rate = 3e-5
 w_decay = 0.01
-#%%
+
+
+
 df = pd.read_excel(r"C:\Users\alepa\Desktop\Database_NLP_Alessandro.xlsx", header=None)
 
 labels = df.iloc[0, 0::2].tolist()
@@ -22,9 +31,9 @@ speeches = df.iloc[0, 1::2].tolist()
 labels = [int(label) for label in labels]
 
 data = pd.DataFrame({'label': labels, 'speech': speeches})
-#%%
+
 train_data, test_data = train_test_split(data, test_size=0.2, stratify=data['label'])
-#%%
+
 class SpeechDataset(Dataset):
     def __init__(self, speeches, labels, tokenizer, max_len=512):
         self.speeches = speeches
@@ -55,7 +64,9 @@ class SpeechDataset(Dataset):
             'attention_mask': encoding['attention_mask'].flatten(),
             'labels': torch.tensor(label, dtype=torch.long)
         }
-#%%
+
+
+
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 train_dataset = SpeechDataset(train_data['speech'].tolist(), train_data['label'].tolist(), tokenizer)
@@ -67,31 +78,46 @@ test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
 model.train()
 model.to(device)
-#%%
+
+
+
 optimizer = torch.optim.AdamW(model.parameters(),
                               lr = l_rate, 
                               weight_decay = w_decay)
-#%%
-def live_plot(train_data, test_data, figsize=(7,5), title=''):
-    clear_output(wait=True)
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=figsize)
-    ax1.plot(train_data['Train Loss'], label='Train Loss')
-    ax1.plot(train_data['Train Accuracy'], label='Train Accuracy')
-    ax1.set_title('Training Loss and Accuracy')
-    ax1.set_xlabel('Epoch')
-    ax1.set_ylabel('Value')
-    ax1.legend(loc='upper right')
-    ax1.grid(True)
 
-    ax2.plot(test_data['Test Loss'], label='Test Loss')
-    ax2.plot(test_data['Test Accuracy'], label='Test Accuracy')
-    ax2.set_title('Testing Loss and Accuracy')
+
+
+def live_plot(train_data, test_data, figsize=(10, 5), title=''):
+    clear_output(wait=True)
+    
+    df_train = pd.DataFrame(train_data)
+    df_train['Type'] = 'Train'
+    df_test = pd.DataFrame(test_data)
+    df_test['Type'] = 'Test'
+    df = pd.concat([df_train, df_test], axis=0)
+    df.reset_index(inplace=True)
+    df.rename(columns={'index': 'Epoch'}, inplace=True)
+
+    sns.set(style='dark')
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=figsize)
+
+    sns.lineplot(x='Epoch', y='Loss', hue='Type',
+                 data=df.melt(id_vars=['Epoch', 'Type'], value_vars=['Train Loss', 'Test Loss'], var_name='Metric',
+                              value_name='Loss'), ax=ax1)
+    ax1.set_title('Loss')
+    ax1.set_xlabel('Epoch')
+    ax1.legend(loc='upper right')
+
+    sns.lineplot(x='Epoch', y='Accuracy', hue='Type',
+                 data=df.melt(id_vars=['Epoch', 'Type'], value_vars=['Train Accuracy', 'Test Accuracy'],
+                              var_name='Metric', value_name='Accuracy'), ax=ax2)
+    ax2.set_title('Accuracy')
     ax2.set_xlabel('Epoch')
-    ax2.set_ylabel('Value')
-    ax2.legend(loc='upper right')
-    ax2.grid(True)
+    ax2.legend(loc='lower right')
 
     plt.suptitle(title)
+    plt.tight_layout()
     plt.show()
 
 train_metrics = {'Train Loss': [], 'Train Accuracy': []}
@@ -99,7 +125,10 @@ test_metrics = {'Test Loss': [], 'Test Accuracy': []}
 
 train_losses, train_accuracies = [], []
 test_losses, test_accuracies = [], []
-#%%
+
+
+
+
 for epoch in range(n_epochs):
     model.train()
     total_train_loss, total_train_accuracy = 0, 0
